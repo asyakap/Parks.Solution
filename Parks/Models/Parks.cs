a30ba861da813d1735830103e981dd8068b94787
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-03-31 08:57:53 -0700

    add parks model

---
+++ Parks/Models/Parks.cs
@@ -0,0 +1,17 @@
+namespace Parks.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Location { get; set; }
+
+    public string Description { get; set; }
+
+    public string InterestingFacts { get; set; }
+
+    public string PopularSights { get; set; }
+  }
+}
